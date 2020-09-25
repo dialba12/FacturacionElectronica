79c8bcd468b599581f57a91d68f128133fca9047
dialba666 <58569295+dialba666@users.noreply.github.com>
2020-09-24 22:30:12 -0600

    Agregar archivos de proyecto.

---
+++ FacturacionElectronica.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FacturacionElectronica.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
